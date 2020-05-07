c307f05290ed7dd0354afbad983ef869833d8cdd
masaki.arizuka <masaki.arizuka@gmail.com>
2020-05-07 10:14:31 +0900

    step27

---
+++ steps/step27.py
@@ -0,0 +1,48 @@
+if '__file__' in globals():
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import numpy as np
+from dezero import Variable, Function
+
+class Sin(Function):
+    def forward(self, x):
+        y = np.sin(x)
+        return y
+
+    def backward(self, gy):
+        x = self.inputs[0].data
+        gx = gy * np.cos(x)
+        return gx
+
+
+def sin(x):
+    return Sin()(x)
+
+x = Variable(np.array(np.pi/4))
+y = sin(x)
+y.backward()
+
+print(y.data)
+print(x.grad)
+
+import math
+
+def my_sin(x, threshold=0.0001):
+    """マクローリン展開によるsin関数の近似
+    """
+    y = 0
+    for i in range(100000):
+        c = (-1) ** i / math.factorial(2*i + 1)
+        t = c*x ** (2*i + 1)
+        y = y + t
+        if abs(t.data) < threshold:
+            break
+    return y
+
+x = Variable(np.array(np.pi/4))
+y = my_sin(x)
+y.backward()
+
+print(y.data)
+print(x.grad)
